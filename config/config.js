163519ca63c569eb74cb9c37a3d442b3c09f60d2
rubeniab <rub_isai@hotmail.com>
2025-08-05 15:53:17 -0600

    Corrección de .env y config

--- config/config.js
+++ config/config.js
@@ -1,26 +1,16 @@
+require('dotenv').config();
+
+const shared = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql'
+};
+
 module.exports = {
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql'
-    },
-    test: {
-        username: process.env.TEST_DB_USER,
-        password: process.env.TEST_DB_PASSWORD,
-        database: process.env.TEST_DB_DATABASE,
-        host: process.env.TEST_DB_HOST,
-        port: process.env.TEST_DB_PORT,
-        dialect: 'mysql'
-    },
-    production: {
-        username: process.env.PROD_DB_USER,
-        password: process.env.PROD_DB_PASSWORD,
-        database: process.env.PROD_DB_DATABASE,
-        host: process.env.PROD_DB_HOST,
-        port: process.env.PROD_DB_PORT,
-        dialect: 'mysql' 
-    }
-}
+  development: shared,
+  production: shared,
+  test: shared
+};
